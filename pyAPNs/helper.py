b4b34b9edab261d4e77a9cbd7e590338c308d0df
Phineas Guo <31276761+guoPhineas@users.noreply.github.com>
2025-12-31 12:02:31 +0800

    Update helper.py

--- pyAPNs/helper.py
+++ pyAPNs/helper.py
@@ -8,35 +8,9 @@ def checkResponse(response:Response)->tuple[int,str,str]:
         case 200:
             return (200,'Success.',response.headers.get('apns-id'))
 
-        case 400:
-            return (400,response.json().get('reason'),response.headers.get('apns-id'))
-
-        case 403:
-            return (403,response.json().get('reason'),response.headers.get('apns-id'))
-
-        case 404:
-            return (404,response.json().get('reason'),response.headers.get('apns-id'))
-
-        case 405:
-            return (405,response.json().get('reason'),response.headers.get('apns-id'))
-
-        case 410:
-            return (410,response.json().get('reason'),response.headers.get('apns-id'))
-
-        case 413:
-            return (413,response.json().get('reason'),response.headers.get('apns-id'))
-
-        case 429:
-            return (429,response.json().get('reason'),response.headers.get('apns-id'))
-
-        case 500:
-            return (500,response.json().get('reason'),response.headers.get('apns-id'))
-
-        case 503:
-            return (503,response.json().get('reason'),response.headers.get('apns-id'))
-
         case _:
-            return (-1,'Unknown.','','')
+            return (response.status_code,response.json().get('reason'),response.headers.get('apns-id'))
+
         
 def APNSRequest(url,body,headers):
     with Client(http2=True) as client:
