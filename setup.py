a7e54435c5b788c0b862edcc70f4483f0ae00b68
Phineas Guo <31276761+guoPhineas@users.noreply.github.com>
2026-01-01 10:13:36 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def parse_requirements(filename):
 
 setuptools.setup(
     name="pyAPNsKit",
-    version="0.1.1",
+    version="0.1.2",
     author="Zonglin Phineas Guo",
     description="Send requests to Apple Push Notification service (APNs) to push notifications to users.",
     long_description=long_description,
